b2cc7a2169ffe655b6d2abf4be78a998699b28b6
Steven-S1020 <stevenstokees1020@gmail.com>
2024-05-02 00:43:00 -0400

    Added Gif folders and FramePull

--- SquishmellowPet.py
+++ SquishmellowPet.py
@@ -1,8 +1,8 @@
 import pyautogui
 import random
 import tkinter as tk
+from PIL import Image
 
-path = '/Gifs/'
 
 def main() -> None:
     window = tk.Tk()
@@ -16,19 +16,33 @@ def main() -> None:
     Squishmellows = {'Manny':manny, 'Chip':chip, 'Timmy':timmy, 'Berry':berry, 'Pip':pip}
     Actions = GifPull(Squishmellows)
 
+    # Fixes window look
+    window.config(hiughlightbackground='black')
+    window.overrideredirect(True)
+    window.wm_attributes('-transparentcolor','black')
+
+    for Squish in Squishmellows:
+        Squish = tk.Label(window,bd=0,bg='black')
+        Squish.pack()
+        window.mainloop()
 
 
 
 def GifPull(Squishmellows):
     for Squish in Squishmellows:
-        idle = [tk.PhotoImage(file=path + Squish + 'idle.gif', format = 'gif --index %i'%(i)) for i in range()]
-        idle_to_sleep = [tk.PhotoImage(file=path + Squish + 'idle_to_sleep.gif', format = 'gif --index %i'%(i)) for i in range()]
-        sleep = [tk.PhotoImage(file=path + Squish + 'sleep.gif', format = 'gif --index %i'%(i)) for i in range()]
-        sleep_to_idle =[tk.PhotoImage(file=path + Squish + 'sleep_to_idle.gif', format = 'gif --index %i'%(i)) for i in range()]
-        walk_pos =[tk.PhotoImage(file=path + Squish + 'walk_pos.gif', format = 'gif --index %i'%(i)) for i in range()]
-        walk_neg =[tk.PhotoImage(file=path + Squish + 'walk_neg.gif', format = 'gif --index %i'%(i)) for i in range()]
+        FilePath = '/Gifs/' + Squish + '/' + Squish
+        idle = [tk.PhotoImage(file=FilePath+'idle.gif', format='gif --index %i' % (i)) for i in range(FramePull(FilePath+'idle.gif'))]
+        idle_to_sleep = [tk.PhotoImage(file=FilePath+'idle_to_sleep.gif', format='gif --index %i' % (i)) for i in FramePull(FilePath+'idle_to_sleep.gif')]
+        sleep = [tk.PhotoImage(file=FilePath+'sleep.gif', format='gif --index %i' % (i)) for i in FramePull(FilePath+'sleep.gif')]
+        sleep_to_idle = [tk.PhotoImage(file=FilePath+'sleep_to_idle.gif', format='gif --index %i' % (i)) for i in FramePull(FilePath+'sleep_to_idle.gif')]
+        walk_pos = [tk.PhotoImage(file=FilePath+'walk_pos.gif', format='gif --index %i' % (i)) for i in FramePull(FilePath+'walk_pos.gif')]
+        walk_neg = [tk.PhotoImage(file=FilePath+'walk_neg.gif',format='gif --index %i' % (i)) for i in FramePull(FilePath+'walk_neg.gif')]
         f'{Squish}'.append(idle, idle_to_sleep, sleep, sleep_to_idle, walk_pos, walk_neg)
 
+
+def FramePull(FilePath):
+    Gif = Image.open(FilePath)
+    return Gif.n_frames
 
 if __name__ == '__main__':
     main()
